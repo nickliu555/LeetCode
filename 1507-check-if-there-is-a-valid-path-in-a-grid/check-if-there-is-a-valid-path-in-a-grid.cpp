284b39b95e4cecc60bc232daafbe6fb2a836d69a
nickliu555 <nickliu555@gmail.com>
2026-04-28 22:51:30 -0400

    Time: 892 ms (5.00%) | Memory: 359.1 MB (5.01%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.cpp
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -1,15 +1,15 @@
 class Solution {
 private:
-    map<pair<int,int>, pair<int,int>> parent;
+    unordered_map<int, int> parent;
 
-    pair<int,int> find(pair<int,int> x) {
+    int find(int x) {
         if (parent[x] == x) return x;
         return parent[x] = find(parent[x]);
     }
 
-    void combine(pair<int,int> x, pair<int,int> y) {
-        pair<int,int> groupX = find(x);
-        pair<int,int> groupY = find(y);
+    void combine(int x, int y) {
+        int groupX = find(x);
+        int groupY = find(y);
         if (groupX != groupY) {
             parent[groupX] = groupY;
         }
@@ -20,27 +20,32 @@ public:
         int m = grid.size(), n = grid[0].size();
         for (int r=-1; r<=2*m+1; ++r) {
             for (int c=-1; c<=2*n+1; ++c) {
-                parent[{r,c}] = {r,c};
+                int cellIndex = getCellIndex(r,c,2*n);
+                parent[cellIndex] = cellIndex;
             }
         }
 
         for (int r=0; r<m; ++r) {
             for (int c=0; c<n; ++c) {
                 if (grid[r][c] == 2 || grid[r][c] == 5 || grid[r][c] == 6) {
-                    combine({2*r, 2*c}, {2*r-1, 2*c});
+                    combine(getCellIndex(2*r, 2*c, 2*n), getCellIndex(2*r-1, 2*c, 2*n));
                 }
                 if (grid[r][c] == 1 || grid[r][c] == 3 || grid[r][c] == 5) {
-                    combine({2*r, 2*c}, {2*r, 2*c-1});
+                    combine(getCellIndex(2*r, 2*c, 2*n), getCellIndex(2*r, 2*c-1, 2*n));
                 }
                 if (grid[r][c] == 2 || grid[r][c] == 3 || grid[r][c] == 4) {
-                    combine({2*r, 2*c}, {2*r+1, 2*c});
+                    combine(getCellIndex(2*r, 2*c, 2*n), getCellIndex(2*r+1, 2*c, 2*n));
                 }
                 if (grid[r][c] == 1 || grid[r][c] == 4 || grid[r][c] == 6) {
-                    combine({2*r, 2*c}, {2*r, 2*c+1});
+                    combine(getCellIndex(2*r, 2*c, 2*n), getCellIndex(2*r, 2*c+1, 2*n));
                 }
             }
         }
 
-        return find({0,0}) == find({2*m-2, 2*n-2});
+        return find(getCellIndex(0, 0, 2*n)) == find(getCellIndex(2*m-2, 2*n-2, 2*n));
+    }
+
+    int getCellIndex(int i, int j, int c) {
+        return i * c + j;
     }
 };
