f197fbfaa4a222d9c1250cca69c5f68c0666d75b
nickliu555 <nickliu555@gmail.com>
2026-05-02 11:13:00 -0400

    Time: 288 ms (5.09%) | Memory: 108.1 MB (5.09%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -2,34 +2,47 @@ class Solution {
 public:
     int rotatedDigits(int n) {
         int ans = 0;
-        unordered_map<int, bool> hasBeenValid;
+        // dp[i] = 0, invalid number
+        // dp[i] = 1, valid and same number
+        // dp[i] = 2, valid and different number
+        unordered_map<int, int> dp;
         for (int num=1; num<=n; ++num) {
-            if (isValid(num, hasBeenValid)) {
+            if (isValid(num, dp)) {
                 ++ans;
             }
         }
         return ans;
     }
 
-    bool isValid(int n, unordered_map<int, bool>& hasBeenValid) {
-        bool validFound = false;
+    bool isValid(int n, unordered_map<int, int>& dp) {
+        bool diffFound = false;
         unordered_set<int> rotatable = {2, 5, 6 , 9};
         unordered_set<int> unrotatable = {3, 4, 7};
+        int originalNum = n;
         while (n > 0) {
-            if (hasBeenValid.count(n) > 0) {
-                validFound = validFound || hasBeenValid[n];
-                break;
+            if (dp.count(n) > 0) {
+                if (dp[n] == 0) {
+                    dp[originalNum] = 0;
+                    return false;
+                } else if (dp[n] == 1) {
+                    dp[originalNum] = diffFound ? 2 : 1;
+                    return dp[originalNum] == 2;
+                } else {
+                    dp[originalNum] = 2;
+                    return true;
+                }
             }
 
             int digit = n % 10;
             if (rotatable.count(digit) > 0) {
-                validFound = true;
+                diffFound = true;
             } else if (unrotatable.count(digit) > 0) {
+                dp[originalNum] = 0;
                 return false;
             }
             n /= 10;
         }
-        hasBeenValid[n] = validFound;
-        return validFound;
+        dp[n] = diffFound ? 2 : 1;
+        return diffFound;
     }
 };
