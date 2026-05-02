c97a6e4a5a1ee0b5c0083a8bb980ef2d0c097593
nickliu555 <nickliu555@gmail.com>
2026-05-02 11:13:23 -0400

    Time: 281 ms (5.09%) | Memory: 100.9 MB (5.09%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -2,47 +2,34 @@ class Solution {
 public:
     int rotatedDigits(int n) {
         int ans = 0;
-        // dp[i] = 0, invalid number
-        // dp[i] = 1, valid and same number
-        // dp[i] = 2, valid and different number
-        unordered_map<int, int> dp;
+        unordered_map<int, bool> hasBeenValid;
         for (int num=1; num<=n; ++num) {
-            if (isValid(num, dp)) {
+            if (isValid(num, hasBeenValid)) {
                 ++ans;
             }
         }
         return ans;
     }
 
-    bool isValid(int n, unordered_map<int, int>& dp) {
-        bool diffFound = false;
+    bool isValid(int n, unordered_map<int, bool>& hasBeenValid) {
+        bool validFound = false;
         unordered_set<int> rotatable = {2, 5, 6 , 9};
         unordered_set<int> unrotatable = {3, 4, 7};
-        int originalNum = n;
         while (n > 0) {
-            if (dp.count(n) > 0) {
-                if (dp[n] == 0) {
-                    dp[originalNum] = 0;
-                    return false;
-                } else if (dp[n] == 1) {
-                    dp[originalNum] = diffFound ? 2 : 1;
-                    return dp[originalNum] == 2;
-                } else {
-                    dp[originalNum] = 2;
-                    return true;
-                }
+            if (hasBeenValid.count(n) > 0) {
+                validFound = validFound || hasBeenValid[n];
+                break;
             }
 
             int digit = n % 10;
             if (rotatable.count(digit) > 0) {
-                diffFound = true;
+                validFound = true;
             } else if (unrotatable.count(digit) > 0) {
-                dp[originalNum] = 0;
                 return false;
             }
             n /= 10;
         }
-        dp[n] = diffFound ? 2 : 1;
-        return diffFound;
+        hasBeenValid[n] = validFound;
+        return validFound;
     }
 };
