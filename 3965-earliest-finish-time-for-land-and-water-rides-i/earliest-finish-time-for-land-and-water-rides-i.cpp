8f61579a82afccc5ea69e278f8f946cdf55c57c9
nickliu555 <nickliu555@gmail.com>
2026-06-01 22:19:59 -0400

    Time: 0 ms (100.00%) | Memory: 92 MB (24.47%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int landStartMin = INT_MAX;
-        for (int i=0; i<landStartTime.size(); ++i) {
-            landStartMin = std::min(landStartMin, landStartTime[i]+landDuration[i]);
+        return std::min(calcFinishTime(landStartTime, landDuration, waterStartTime, waterDuration), calcFinishTime(waterStartTime, waterDuration, landStartTime, landDuration));
+    }
+
+    int calcFinishTime(vector<int>& startTime1, vector<int>& duration1, vector<int>& startTime2, vector<int>& duration2) {
+        int startMin = INT_MAX;
+        for (int i=0; i<startTime1.size(); ++i) {
+            startMin = std::min(startMin, startTime1[i]+duration1[i]);
         }
         int ans = INT_MAX;
-        for (int i=0; i<waterStartTime.size(); ++i) {
-            ans = std::min(ans, std::max(landStartMin, waterStartTime[i]) + waterDuration[i]);
+        for (int i=0; i<startTime2.size(); ++i) {
+            ans = std::min(ans, std::max(startMin, startTime2[i]) + duration2[i]);
         }
-        
-        int seaStartMin = INT_MAX;
-        for (int i=0; i<waterStartTime.size(); ++i) {
-            seaStartMin = std::min(seaStartMin, waterStartTime[i]+waterDuration[i]);
-        }
-        for (int i=0; i<landStartTime.size(); ++i) {
-            ans = std::min(ans, std::max(seaStartMin, landStartTime[i]) + landDuration[i]);
-        }
-
         return ans;
     }   
 };
