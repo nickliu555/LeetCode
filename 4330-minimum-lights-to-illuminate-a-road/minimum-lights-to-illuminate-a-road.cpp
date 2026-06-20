e323c3fa51f61208f0a59c38a5493442a6d27098
nickliu555 <nickliu555@gmail.com>
2026-06-20 15:51:55 -0400

    Time: 28 ms (50.00%) | Memory: 277.9 MB (60.00%) - LeetSync

---
+++ 4330-minimum-lights-to-illuminate-a-road/minimum-lights-to-illuminate-a-road.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minLights(vector<int>& lights) {
+        int n = lights.size();
+        vector<int> lightIncr(n, 0);
+        for (int i=0; i<n; ++i) {
+            if (lights[i] > 0) {
+                ++lightIncr[std::max(0, i-lights[i])];
+                if (i+lights[i]+1 < n) {
+                    --lightIncr[i+lights[i]+1];
+                }
+            }
+        }
+
+        int numInstall = 0, currLight = 0;
+        for (int i=0; i<n; ++i) {
+            currLight += lightIncr[i];
+            if (currLight == 0) {
+                ++numInstall;
+                ++currLight;
+                if (i+3 < n) {
+                    --lightIncr[i+3];
+                }
+            }
+        }
+        return numInstall;
+    }
+};
