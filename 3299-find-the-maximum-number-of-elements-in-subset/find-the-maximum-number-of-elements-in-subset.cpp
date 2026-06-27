2be42021b5dacce0e3c8455da2554ebde18e3136
nickliu555 <nickliu555@gmail.com>
2026-06-27 16:36:04 -0400

    Time: 123 ms (35.32%) | Memory: 135.4 MB (9.95%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
@@ -4,21 +4,30 @@ public:
         unordered_map<int, int> count;
         for (int num: nums) ++count[num];
         
-        std::sort(nums.begin(), nums.end(), greater<int>());
         unordered_map<int, int> dp;
         int ans = 1;
         for (int num: nums) {
+            if (dp[num] > 0) continue;
+
             if (num == 1) {
                 ans = std::max(ans, count[1] - (count[1]%2==0));
             } else {
-                dp[num] = 1;
-                if (count[num] >= 2 && num <= std::sqrt(std::numeric_limits<int>::max())) {
-                    int squared = num * num;
-                    dp[num] = dp[squared] + 1;
-                    ans = std::max(ans, dp[num]*2-1);
-                }
+                int len = dfs(num, count, dp);
+                ans = std::max(ans, len*2-1);
             }
         }
+        return ans;
+    }
+
+    int dfs(int num, unordered_map<int, int>& count, unordered_map<int, int>& dp) {
+        if (count[num] == 0) return 0;
+
+        int ans = 1;
+        if (count[num] >= 2 && num <= std::sqrt(std::numeric_limits<int>::max())) {
+            int squared = num * num;
+            ans = 1 + dfs(squared, count, dp);
+        }
+        dp[num] = ans;
         return ans;
     }
 };
