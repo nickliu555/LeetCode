3f1b8083e7d10d3a28637b589020013c6c91423f
nickliu555 <nickliu555@gmail.com>
2026-07-18 14:40:45 -0400

    Time: 14 ms (76.79%) | Memory: 21.9 MB (75.89%) - LeetSync

---
+++ 320-generalized-abbreviation/generalized-abbreviation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> generateAbbreviations(string word) {
+        vector<string> ans;
+        helper("", 0, word, ans);
+        return ans;
+    }
+
+    void helper(string currAbbr, int index, string word, vector<string>& ans) {
+        if (index >= word.length()) {
+            ans.push_back(currAbbr);
+            return;
+        }
+
+        if (currAbbr.empty() || !std::isdigit(currAbbr.back())) {
+            int maxLen = word.length() - index;
+            for (int len=1; len<=maxLen; ++len) {
+                helper(currAbbr+std::to_string(len), index+len, word, ans);
+            }
+        }
+        helper(currAbbr+word[index], index+1, word, ans);
+    }
+};
