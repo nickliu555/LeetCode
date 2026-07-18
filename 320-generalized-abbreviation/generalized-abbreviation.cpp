9c3babe4be19858b85ca5caff0deb941cb5cbf33
nickliu555 <nickliu555@gmail.com>
2026-07-18 14:42:04 -0400

    Time: 16 ms (62.95%) | Memory: 21.9 MB (75.89%) - LeetSync

--- 320-generalized-abbreviation/generalized-abbreviation.cpp
+++ 320-generalized-abbreviation/generalized-abbreviation.cpp
@@ -12,12 +12,14 @@ public:
             return;
         }
 
+        // use length to abbreviate
         if (currAbbr.empty() || !std::isdigit(currAbbr.back())) {
             int maxLen = word.length() - index;
             for (int len=1; len<=maxLen; ++len) {
                 helper(currAbbr+std::to_string(len), index+len, word, ans);
             }
         }
+        // just use the letters
         helper(currAbbr+word[index], index+1, word, ans);
     }
 };
