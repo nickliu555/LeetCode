0673e7aa55e2037e2922bb898162b6a6feda721e
nickliu555 <nickliu555@gmail.com>
2026-07-21 07:42:09 -0400

    Time: 46 ms (84.23%) | Memory: 54.8 MB (90.54%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
         int numActive = 0, maxDelta = 0, prevInactive = 0, postInactive = 0, n = s.length();
-        s = '1' + s + '1';
-        for (int i=1; i<=n; ++i) {
+        for (int i=0; i<n; ++i) {
             if (s[i] == '1') {
                 ++numActive;
             } else {
-                if (s[i-1] == '1') {
+                if (i == 0 || s[i-1] == '1') {
                     prevInactive = postInactive;
                     postInactive = 0;
                 }
